032f2117f65340b25f74d27227c3d78ca77d1baa
hoanglt <hcmdev2021@gmail.com>
2022-03-23 16:57:35 +0700

    wip (UI): ENGEVA-5 メンバーが評価フォームから評価を3点登録することができる
    config api

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- front-end/src/app/sample1/sample1.component.ts
+++ front-end/src/app/sample1/sample1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from 'src/api/sampleConfig/config.service';
 
 @Component({
   selector: 'app-sample1',
@@ -6,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sample1.component.scss'],
 })
 export class Sample1Component implements OnInit {
-  constructor() {}
+  constructor(private configService: ConfigService) {}
 
   ngOnInit(): void {
+    this.configService.getConfig().subscribe(data => console.log('data', data));
     console.log('sample 1');
   }
 }
